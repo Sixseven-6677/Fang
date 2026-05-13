55b9ffaf6c2f316921e7dc4b549be7a04ab8c000
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-13 06:20:18 +0200

    fix(welcome): fix syntax error + empty run

--- SCRIPTS/ZAO-EVTS/join.js
+++ SCRIPTS/ZAO-EVTS/join.js
@@ -1,10 +1,25 @@
-
 module.exports = {
-	config: {
-		name: "welcome",
-		Type: ["log:subscribe"],
-		Owner: "GryKJ"
-	},
-	run: async ({ event, api }) => {
-		
+  config: {
+    name: "welcome",
+    eventType: ["log:subscribe"],
+    Owner: "GryKJ"
+  },
+  run: async ({ event, api }) => {
+    try {
+      const { threadID } = event;
+      if (
+        event.logMessageData &&
+        event.logMessageData.addedParticipants &&
+        event.logMessageData.addedParticipants.some(
+          (p) => p.userFbId == api.getCurrentUserID()
+        )
+      ) {
+        api.changeNickname(
+          `» ${global.config.PREFIX} « → ${global.config.BOTNAME || "ZAO Bot"}`,
+          threadID,
+          api.getCurrentUserID()
+        );
+      }
+    } catch (e) {}
+  }
 };
