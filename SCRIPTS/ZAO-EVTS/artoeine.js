69003e25c02f48521cb75ac48a5c25e361688f96
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-13 06:20:19 +0200

    fix(antiout): proper messages + error handling

--- SCRIPTS/ZAO-EVTS/artoeine.js
+++ SCRIPTS/ZAO-EVTS/artoeine.js
@@ -1,22 +1,38 @@
 module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
- version: "0.0.1",
+ version: "0.0.2",
  credits: "DungUwU",
- description: "Listen events"
+ description: "Anti-leave"
 };
 
-module.exports.run = async({ event, api, Threads, Users }) => {
- let data = (await Threads.getData(event.threadID)).data || {};
- if (data.antiout == false) return;
- if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
- const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
- if (type == "self-separation") {
-  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
-   if (error) {
-    api.sendMessage(` ${name} 👿🔥 سـحـقـاً لـقـد افـلـت/`, event.threadID)
-   } else api.sendMessage(``, event.threadID);
-  })
- }
-}
+module.exports.run = async ({ event, api, Threads, Users }) => {
+  try {
+    let data = (await Threads.getData(event.threadID)).data || {};
+    if (data.antiout == false) return;
+    if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+    const name = global.data.userName.get(event.logMessageData.leftParticipantFbId)
+      || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+    const type = (event.author == event.logMessageData.leftParticipantFbId)
+      ? "self-separation" : "being kicked";
+    if (type == "self-separation") {
+      api.addUserToGroup(
+        event.logMessageData.leftParticipantFbId,
+        event.threadID,
+        (error) => {
+          if (error) {
+            api.sendMessage(
+              `😈 ${name} | يمنع الهروب في حضور السيد الشاب 🔥`,
+              event.threadID
+            );
+          } else {
+            api.sendMessage(
+              `🩸 ${name} حاول الهروب... تم استدعاؤه مجدداً 😈`,
+              event.threadID
+            );
+          }
+        }
+      );
+    }
+  } catch (e) {}
+};
